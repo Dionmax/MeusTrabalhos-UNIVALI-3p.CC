ef730432d9088d93e3ff41a29236a3deec9fd4f0
Dionmax Nobre <dionmax@edu.univali.br>
2018-02-28 20:58:18 -0300

    Começo

---
+++ TesteAula1-POB/src/testeaula1/pob/TesteAula1POB.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testeaula1.pob;
+
+/**
+ *
+ * @author Dionmax
+ */
+public class TesteAula1POB {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static class ComecoPrograma {
+    
+}
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
