a6ed35cc3a096c28cd470d247c56ce39e7ea37ef
Dionmax Nobre <dionmax@edu.univali.br>
2018-02-28 19:44:23 -0300

    Começo2

--- TesteAula-ED/TesteAula-ED/ListaEstatica.h
+++ TesteAula-ED/TesteAula-ED/ListaEstatica.h
@@ -1,3 +1,18 @@
 #pragma once
 
+#include <iostream>
+#include "string"
+
+using namespace std;
+
+struct Baralho
+{
+	int numeroCartas;
+	string naipeCartas;
+};
+
+struct ListaDeCartas
+{
+	Baralho quantidadeDeCartas;
+};
 
