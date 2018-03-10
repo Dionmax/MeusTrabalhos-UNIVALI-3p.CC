b7e5ba01ffbe3d3be85cded9b574cbfe0a12c801
Dionmax Nobre <dionmax@edu.univali.br>
2018-03-09 21:32:25 -0300

    Implemento de funções.

--- ListaEncadeadaMusicas/ListaEncadeadaMusicas/ListaEncadeadaMusica.h
+++ ListaEncadeadaMusicas/ListaEncadeadaMusicas/ListaEncadeadaMusica.h
@@ -32,20 +32,57 @@ void inicializar(Playlist & lista)
 
 bool insirirInicioDaPlaylistEscadeada(Playlist & lista, ListaMusicas musica)
 {
-	bool EspacoLivre = false;
+	bool EspacoLivre = true;
 
 	Elemento * novo = new Elemento;
 
 	novo->musica = musica;
 
-	if (novo != NULL)
-		EspacoLivre = true;
-
-	novo->proxima = lista.inicio;
-
-	lista.inicio = novo;
-	lista.quantidade++;
+	if (novo == NULL)
+		EspacoLivre = false;
+	else
+	{
+		novo->proxima = lista.inicio;
+		lista.inicio = novo;
+		lista.quantidade++;
+	}
 
 	return EspacoLivre;
+}
+
+bool insirirFinalDaLista(Playlist & lista, ListaMusicas musica)
+{
+	bool EspacoLivre = true;
+
+	if (lista.quantidade == 0)
+		insirirInicioDaPlaylistEscadeada(lista, musica);
+
+	Elemento * novo = new Elemento;
+
+	if (novo == NULL)
+		EspacoLivre = false;
+	else
+	{
+		novo->musica = musica;
+		novo->proxima = NULL;
+
+		Elemento * ultimo;
+
+		ultimo->proxima = lista.inicio;
+
+		while (ultimo->proxima != NULL)
+			ultimo = ultimo->proxima;
+
+		ultimo->proxima = novo;
+
+		lista.quantidade++;
+	}
+
+	return EspacoLivre;
+}
+
+bool removerInicioPlaylistEncadeada(Playlist & lista, ListaMusicas musica)
+{
+
 }
 
