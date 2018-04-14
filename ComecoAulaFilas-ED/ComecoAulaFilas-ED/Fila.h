d4dd92046a497e86e6bc08ac12be60098f359df1
Dionmax Nobre <dionmax@edu.univali.br>
2018-04-13 21:23:02 -0300

    MeioAula!

--- ComecoAulaFilas-ED/ComecoAulaFilas-ED/Fila.h
+++ ComecoAulaFilas-ED/ComecoAulaFilas-ED/Fila.h
@@ -30,5 +30,22 @@ void inicializarFila(TFila<T> fila)
 template<typename T>
 bool enfileirar(TFila<T> fila, T obj)
 {
-	
+	bool verificacao = false;
+
+	ElementoF<T> *novo = new ElementoF<T>;
+
+	if (novo != NULL)
+		verificacao = true;
+
+	novo->objeto = obj;
+
+	novo->proximo = NULL;
+
+	fila.fim->proximo = novo;
+
+	fila.fim = novo;
+
+	fila.quantidade++;
+
+	return verificacao;
 }
